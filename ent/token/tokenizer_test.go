f0e2195bd60a8651a7d3a5d0f5bea5966eac5427
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-09-09 12:51:51 -0500

    update example

--- ent/token/tokenizer_test.go
+++ ent/token/tokenizer_test.go
@@ -88,7 +88,7 @@ func Example() {
 	text := "one\vtwo Poma-  \t\r\n tomus " +
 		"dash -\nstandalone " +
 		"Tora-\nBora\n\rthree 1778,\n"
-	res := token.Tokenize([]rune(text), wrapToken)
+	res := token.Tokenize([]rune(text), func(t token.TokenNER) token.TokenNER { return t })
 	fmt.Println(res[0].Cleaned())
 	fmt.Println(res[2].Cleaned())
 	// Output:
